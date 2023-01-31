e461c0714534d6217c26dbd49d13f029e862c256
samuelmoiwa <samuelmoiwa@gmail.com>
2023-01-31 13:13:09 +0000

    add navBar.js

---
+++ src/components/navBar.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
+
+const Navbar = () => {
+  return (
+    <div className="nav_header_container">
+      <div className="logo-header">
+        <StaticImage src="../images/logo.png" alt="" className="logo" />
+        <h2 className="logo">Space Travelers Hub</h2>
+      </div>
+
+      <nav className="nav_items">
+        <ul className="nav-links">
+          <li>
+            <Link
+              to="/"
+              className={`nav_link ${(isActive) => (isActive ? 'active' : '')}`}
+              end
+            >
+              Rockets
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/Missions/"
+              className={`nav_link ${(isActive) => (isActive ? 'active' : '')}`}
+            >
+              Missions
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/Profile/"
+              className={`nav_link ${(isActive) => (isActive ? 'active' : '')}`}
+            >
+              My profile
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar
