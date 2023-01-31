005f422d1917d83b66fe1c437a33143f7b1d2d36
samuelmoiwa <samuelmoiwa@gmail.com>
2023-01-31 13:11:19 +0000

    add gatsby-config.js

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
+}
