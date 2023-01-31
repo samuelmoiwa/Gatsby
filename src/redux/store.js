0c951e40d218c9eb96f2fb23213f0b192f4e9b6d
samuelmoiwa <samuelmoiwa@gmail.com>
2023-01-31 17:04:46 +0000

    update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,39 @@ import { configureStore } from '@reduxjs/toolkit';
 import missionSlice from './missions/missionSlice';
 import rocketReducer from './rocket/rockets';
 
-const store = configureStore ({
+const loadState = () => {
+  try {
+  const serializedState = localStorage.getItem('state');
+  if (serializedState === null) {
+  return undefined;
+  }
+  return JSON.parse(serializedState);
+  } catch (err) {
+  return undefined;
+  }
+  };
+
+  const saveState = (state) => {
+  try {
+  const serializedState = JSON.stringify(state);
+  localStorage.setItem('state', serializedState);
+  } catch (err) {
+  // Ignore write errors.
+  }
+  };
+
+  const persistedState = loadState();
+
+  const store = configureStore ({
   reducer: {
-    mission: missionSlice,
-    rockets: rocketReducer,
+  mission: missionSlice,
+  rockets: rocketReducer,
   },
+  preloadedState: persistedState,
+  });
+
+  store.subscribe(() => {
+  saveState(store.getState());
 });
 
-export default store;
+  export default store;
