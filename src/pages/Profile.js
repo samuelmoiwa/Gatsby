bf1ad25f19711c836efb1764a55b7b00dec85b62
samuelmoiwa <samuelmoiwa@gmail.com>
2023-01-31 17:04:33 +0000

    update profile.js

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { leaveMission } from '../redux/missions/missionSlice'
 import { cancelRocket } from '../redux/rocket/rockets'
